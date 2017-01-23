3dbcd373e8d422a41c30dc9b7bbbdde0d3807661
Creative Roderick <CreativeRoderick@gmail.com>
2017-01-22 23:29:03 -0600

    Updated Portfolio Template

---
+++ RodPride/js/pride.js
@@ -0,0 +1,11 @@
+/*******************
+MENU
+*******************/
+$(".button-collapse").sideNav();
+
+/*******************
+SLIDER
+*******************/
+$(document).ready(function(){
+     $('.slider').slider({full_width: true});
+   });
